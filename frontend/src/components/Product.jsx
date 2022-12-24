7f6e1a402c9a9e19ae41e8d5919764b2242551d1
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2022-12-25 01:03:57 +0600

    add Link from react router dom

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,22 +1,25 @@
 // Parent component => Home page
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+// Components
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
     <>
       <Card className="mb-4">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Img src={product.image} variant="top" />
-        </a>
+        </Link>
 
         <Card.Body>
-          <a href={`/product/${product._id}`}>
+          <Link to={`/product/${product._id}`}>
             <Card.Title as="div">
               <strong>{product.name}</strong>
             </Card.Title>
-          </a>
+          </Link>
 
           <Card.Text as="div">
             <Rating
