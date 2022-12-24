e24c059b98922eb10816646adc6be25757035966
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2022-12-25 01:02:40 +0600

    implement react router & add Product details page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import HomePage from './pages/HomePage';
+import ProductDetails from './pages/ProductDetails';
 
 // Components 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// Pages
-import HomePage from './pages/HomePage';
-
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main>
         <Container>
-          <HomePage />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/product/:id' element={<ProductDetails />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
