4661f5c6a42d12e02ed6f412489554818705409c
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2022-12-26 00:07:37 +0600

    initialize dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
 
+// Initialize app
 const app = express()
+
+// Dot evn configaration
+dotenv.config()
 
 app.get('/', (req, res) => {
     res.send("hello world ....");
@@ -17,4 +22,6 @@ app.get('/api/product/:id', (req, res) => {
     res.json(product);
 })
 
-app.listen(5000, console.log('Server is running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
