fe256e5629cea651c71722f771ddabecf984b7fb
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2022-12-25 21:59:47 +0600

    fetching products data by api.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,21 @@
 // Parent Component => App
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import axios from "axios";
 // Component
 import Product from "../components/Product";
-import products from "../products";
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get("/api/products");
+      setProducts(res.data);
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1 className="mb-4">Latest Products</h1>
