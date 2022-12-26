b4b9e61d9829d6af685d03f6324ebbda6660ff83
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2022-12-26 18:48:56 +0600

    connect database function call

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors'
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 // Initialize app
@@ -7,6 +9,9 @@ const app = express()
 
 // Dot evn configaration
 dotenv.config()
+
+// Connected with Database
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("hello world ....");
@@ -24,4 +29,4 @@ app.get('/api/product/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
