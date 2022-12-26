f808efa4fa9b0e9997f26cf32f62544d02b9c855
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2022-12-26 19:51:06 +0600

    create product model.

---
+++ backend/model/productModel.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema({
+    reviewer_name: { type: String, required: [true, 'Reviewer name is required'] },
+    reviewer_rating: { type: Number, required: [true, 'Reviewer rating is required'] },
+    reviewer_comment: { type: String, required: [true, 'Reviewer comment is required'] },
+}, {
+    timestamps: true
+})
+
+const productSchema = mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'User id is required'],
+        ref: 'userData'
+    },
+    product_name: {
+        type: String,
+        required: [true, 'Product name is required']
+    },
+    product_image: {
+        type: String,
+        required: [true, 'Product image is required'],
+    },
+    product_brand: {
+        type: String,
+        required: [true, 'Product brand is required']
+    },
+    product_category: {
+        type: String,
+        required: [true, 'Product category is required'],
+    },
+    product_description: {
+        type: String,
+        required: [true, 'Product description is required'],
+    },
+    product_avg_rating: {
+        type: Number,
+        required: [true, 'Product average rating is required'],
+        default: 0
+    },
+    product_reviews: [reviewSchema],
+    number_of_reviews: {
+        type: Number,
+        required: [true, 'Number of veriews is required'],
+        default: 0
+    },
+    product_price: {
+        type: Number,
+        required: [true, 'Product price is required'],
+        default: 0
+    },
+    product_stock_count: {
+        type: Number,
+        required: [true, 'Product stock count is required'],
+        default: 0
+    }
+}, {
+    timestamps: true
+})
+
+const productData = mongoose.model('productData', productSchema, 'productData');
+
+export default productData;
