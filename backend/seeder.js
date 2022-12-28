83549972536f142885ba886db26b6eb4b64cfc05
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2022-12-29 02:14:48 +0600

    crate seeder.js

---
+++ backend/seeder.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/user.js';
+import products from './data/products.js';
+import userData from './model/userModel.js';
+import productData from './model/productModel.js';
+import orderData from './model/orderModel.js';
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB()
+
+const insertData = async () => {
+    try {
+        const createUsers = await userData.insertMany(users);
+        const adminUser = createUsers[0]._id;
+        const sampleProducts = products.map(product => {
+            return { ...product, user_id: adminUser }
+        });
+
+        await productData.insertMany(sampleProducts);
+
+        console.log("Data Imported!".green.inverse)
+        process.exit()
+    } catch (error) {
+        console.log(`${error}`.red.inverse)
+        process.exit(1)
+    }
+}
+
+const destroyData = async () => {
+    try {
+        await userData.deleteMany()
+        await productData.deleteMany()
+        await orderData.deleteMany()
+
+        console.log("Data Detroyed!".red.inverse)
+        process.exit()
+    } catch (error) {
+        console.log(`${error}`.red.inverse)
+        process.exit(1)
+    }
+}
+
+if (process.argv[2] === '-d') {
+    destroyData()
+} else {
+    insertData()
+}
