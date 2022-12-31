ab4e957a65dda71c2ed86217655d8c6140e65956
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2022-12-31 13:56:00 +0600

    add custom error handler middleware.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 // Controller
@@ -19,8 +20,14 @@ app.get('/', (req, res) => {
     res.send("hello world ....");
 })
 
-app.use('/api/products', products_controller)
+app.use('/api/products', products_controller);
 
+// Error Middleware
+app.use(notFound)
+app.use(errorHandler)
+
+// Port
 const PORT = process.env.PORT || 5000;
 
+// Listening port
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
