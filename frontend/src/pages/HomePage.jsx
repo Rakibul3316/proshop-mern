d493569a4bdce9b6a00bf2d8efc762d6f061c9de
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-02 02:00:04 +0600

    /slices/productSlice to /slices/productsSlice

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@
 import { React, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchProducts } from "../slices/productSlice";
+import { fetchProducts } from "../slices/productsSlice";
 
 // Component
 import Product from "../components/Product";
