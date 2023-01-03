aa20e63e0f62356887dfcae3f17df6c63bf8edb6
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-03 21:30:28 +0600

    add cart slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore, } from '@reduxjs/toolkit';
 import productsSlice from './slices/productsSlice';
 import productDetailsSlice from './slices/productDetailsSlice';
+import cartSlice from './slices/cartSlice';
 
 export const store = configureStore({
     reducer: {
         products: productsSlice,
-        product: productDetailsSlice
+        product: productDetailsSlice,
+        cart: cartSlice
     },
 })
