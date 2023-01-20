5b30791d4726b04d9ab154db8092a16756837606
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-21 03:49:38 +0600

    redesign.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,25 +1,23 @@
-import express from "express";
 import asyncHandler from 'express-async-handler';
-const router = express.Router();
 
-import productData from '../model/productModel.js';
+import productData from '../models/productModel.js';
 
 // @desc        Fetch all products
 // @route       GET /api/products
 // @access      Public
-router.get('/', asyncHandler(async (req, res) => {
+const getProducts = asyncHandler(async (req, res) => {
     const products = await productData.find({});
 
     res.json({
         data: products,
         success: true
     })
-}))
+})
 
 // @desc        Fetch single product
 // @route       GET /api/products/:id
 // @access      Public
-router.get('/:id', asyncHandler(async (req, res) => {
+const getProductById = asyncHandler(async (req, res) => {
     let product = await productData.findById(req.params.id);
 
     if (product) {
@@ -32,6 +30,9 @@ router.get('/:id', asyncHandler(async (req, res) => {
         throw new Error('Product not found!')
     }
 
-}))
+})
 
-export default router;
+export {
+    getProducts,
+    getProductById
+};
