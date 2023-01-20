0a93c5a0ecb3453d29e5ae098a4c22d13c3b897e
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-21 03:48:49 +0600

    add body parser & replace contollers by routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors'
+// Middleware
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
-// Controller
-import products_controller from './controller/productController.js'
+// Routes
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js';
 
 // Initialize app
 const app = express()
+
+// Body parser
+app.use(express.json())
 
 // Dot evn configaration
 dotenv.config()
@@ -16,11 +21,14 @@ dotenv.config()
 // Connected with Database
 connectDB();
 
+// Initila Route
 app.get('/', (req, res) => {
     res.send("hello world ....");
 })
 
-app.use('/api/products', products_controller);
+// All Routes
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 // Error Middleware
 app.use(notFound)
