f23483e6ae87a40057fde2d22a56ccd04f7aa539
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-24 02:15:17 +0600

    add updateUserProfile controller in put route.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,14 +3,15 @@ import express from "express";
 import {
     getUser,
     getUserProfile,
-    registerUser
+    registerUser,
+    updateUserProfile
 } from "../controllers/userController.js";
 // Protect Middleware
 import protect from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post('/login', getUser);
-router.route('/profile').get(protect, getUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 router.post('/', registerUser);
 
 export default router;
