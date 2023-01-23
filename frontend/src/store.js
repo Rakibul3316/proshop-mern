4c6f185c289f4463de7ff06c9bdf1d8c40521752
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-24 02:16:09 +0600

    add userSlice.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,13 @@ import { configureStore, } from '@reduxjs/toolkit';
 import productsSlice from './slices/productsSlice';
 import productDetailsSlice from './slices/productDetailsSlice';
 import cartSlice from './slices/cartSlice';
+import userSlice from './slices/userSlice';
 
 export const store = configureStore({
     reducer: {
         products: productsSlice,
         product: productDetailsSlice,
-        cart: cartSlice
+        cart: cartSlice,
+        user: userSlice
     },
 })
