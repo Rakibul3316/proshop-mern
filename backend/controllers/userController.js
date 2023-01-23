4c0bced6fc3ed46ff8bb0891822b67c5c390c92d
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-24 02:14:45 +0600

    add user update controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -83,4 +83,35 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 })
 
-export { getUser, getUserProfile, registerUser };
+// @desc        Update user profile
+// @route       PUT /api/users/profile
+// @access      Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    // This user information is passed in request in protected middleware.
+    const user = await userData.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        })
+
+    } else {
+        res.status(404) // Not Found
+        throw new Error('User not found.')
+    }
+
+})
+
+export { getUser, getUserProfile, registerUser, updateUserProfile };
