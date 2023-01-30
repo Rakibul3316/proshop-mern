a2b3a91d3dfaf0755f6896242c46c4993e4f618d
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-31 03:29:56 +0600

    add updateOrderToPaid & getMyOrders controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -42,4 +42,38 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
-export { addOrderItems, getOrderById }
+// @desc        Update order to paid
+// @route       GET /api/orders/:id/pay
+// @access      Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await orderData.findById({ '_id': req.params.id })
+
+    if (order) {
+        order.isPaid = true
+        order.paid_at = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+})
+
+// @desc        Get logged in user orders
+// @route       GET /api/orders/myorders
+// @access      Private
+const getMyOrders = asyncHandler(async (req, res) => {
+    const orders = await orderData.find({ 'user_id': req.user._id })
+
+    res.json(orders);
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
