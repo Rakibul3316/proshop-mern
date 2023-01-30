8299b4f2df9cf092b9a5455165448ea092eb82e8
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-30 18:15:23 +0600

    design placeorder page.

---
+++ frontend/src/pages/PlaceOrderPage.jsx
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Image,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { createOrder } from "../slices/orderSlice.js";
+
+// Component
+import Message from "../components/Message.jsx";
+import CheckOutSteps from "../components/CheckOutSteps.jsx";
+
+const PlaceOrderPage = () => {
+  // State
+  const cart = useSelector((state) => state.cart);
+  const { order, success, error } = useSelector((state) => state.order),
+    orderId = order._id;
+
+  const dispatch = useDispatch(),
+    navigate = useNavigate();
+
+  useEffect(() => {
+    if (success) {
+      navigate(`/order/${orderId}`);
+    }
+  }, [success, navigate, orderId]);
+
+  const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2);
+  };
+
+  // Calculate
+  let itemsPrice = addDecimals(
+      cart.cartItems.reduce(
+        (acc, item) => acc + item.product_price * item.qty,
+        0
+      )
+    ),
+    shippingPrice = addDecimals(itemsPrice > 100 ? 0 : 100),
+    taxPrice = addDecimals(Number((0.15 * itemsPrice).toFixed(2))),
+    totalPrice = (
+      Number(itemsPrice) +
+      Number(shippingPrice) +
+      Number(taxPrice)
+    ).toFixed(2);
+
+  const orderData = {
+    orderItems: cart.cartItems,
+    shippingAddress: cart.shippingAddress,
+    paymentMethod: cart.paymentMethod,
+    itemsPrice,
+    shippingPrice,
+    taxPrice,
+    totalPrice,
+  };
+
+  const placeOrderHandler = () => {
+    dispatch(createOrder(orderData));
+  };
+
+  return (
+    <>
+      <CheckOutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address: </strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city}{" "}
+                {cart.shippingAddress.postalCode},{" "}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Method: </strong>
+                {cart.paymentMethod}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {cart.cartItems.map((item, idx) => (
+                    <ListGroup.Item key={item._id}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.product_image}
+                            alt={item.product_name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/products/${item._id}`}>
+                            {item.product_name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} X $ {item.product_price} = $
+                          {item.qty * item.product_price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {error && <Message variant="danger">{error}</Message>}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type="button"
+                  className="btn-block w-100"
+                  disabled={cart.cartItems === 0}
+                  onClick={placeOrderHandler}
+                >
+                  Place Order
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default PlaceOrderPage;
