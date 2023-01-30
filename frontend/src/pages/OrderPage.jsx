fbe1aa75625817838eb70f20b026213e2dfd6364
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-30 18:14:53 +0600

    design order page.

---
+++ frontend/src/pages/OrderPage.jsx
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Image,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { createOrder, getOrderDetails } from "../slices/orderSlice.js";
+
+// Component
+import Message from "../components/Message.jsx";
+import Loader from "../components/Loader.jsx";
+
+const OrderPage = () => {
+  // State
+  const { order, loading, error } = useSelector((state) => state.order);
+  const { id } = useParams();
+
+  console.log("order >>", order);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getOrderDetails(id));
+  }, [dispatch, id]);
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address: </strong>
+                {order.shipping_address.address}, {order.shipping_address.city}{" "}
+                {order.shipping_address.postalCode},{" "}
+                {order.shipping_address.country}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Method: </strong>
+                {order.payment_method}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {order.order_items.length === 0 ? (
+                <Message>Order is empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {order.order_items.map((item, idx) => (
+                    <ListGroup.Item key={item._id}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.product_image}
+                            alt={item.product_name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/products/${item._id}`}>
+                            {item.product_name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} X $ {item.product_price} = $
+                          {item.qty * item.product_price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${order.items_price}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${order.shipping_price}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${order.tax_price}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${order.total_price}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default OrderPage;
