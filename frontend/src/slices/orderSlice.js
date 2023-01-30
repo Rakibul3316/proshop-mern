ca20927898ae23caf0c507e7d317c57a339c0f2c
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-01-31 03:32:59 +0600

    add actions for payment & getMyOrders.

--- frontend/src/slices/orderSlice.js
+++ frontend/src/slices/orderSlice.js
@@ -6,8 +6,7 @@ const initialState = {
     error: null,
     order: {},
     success: false,
-    orderItem: [],
-    shippingAddress: {}
+    myOrders: []
 }
 
 // Actions
@@ -39,12 +38,54 @@ export const getOrderDetails = createAsyncThunk('getOrderDetails', async (id, { 
         const { userInfo } = getState().userLogIn;
         const config = {
             headers: {
-                'Content-Type': 'application/json',
                 Authorization: `Bearer ${userInfo.token}`,
             }
         }
 
         const { data } = await axios.get(`/api/orders/${id}`, config);
+        return data;
+    } catch (error) {
+        if (error.response && error.response.data.message) {
+            return rejectWithValue(error.response.data.message);
+        } else {
+            return rejectWithValue(error.message);
+        }
+    }
+}
+)
+
+export const payOrder = createAsyncThunk('payOrder', async ({ orderId, paymentResult }, { getState, rejectWithValue, dispatch }) => {
+    try {
+        const { userInfo } = getState().userLogIn;
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`,
+            }
+        }
+
+        const { data } = await axios.put(`/api/orders/${orderId}/pay`, paymentResult, config);
+        return data;
+    } catch (error) {
+        if (error.response && error.response.data.message) {
+            return rejectWithValue(error.response.data.message);
+        } else {
+            return rejectWithValue(error.message);
+        }
+    }
+}
+)
+
+export const getMyOrders = createAsyncThunk('getMyOrders', async (_, { getState, rejectWithValue, dispatch }) => {
+    try {
+        const { userInfo } = getState().userLogIn;
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            }
+        }
+
+        const { data } = await axios.get(`/api/orders/myorders`, config);
         return data;
     } catch (error) {
         if (error.response && error.response.data.message) {
@@ -85,6 +126,30 @@ export const orderSlice = createSlice({
                 state.order = action.payload;
             })
             .addCase(getOrderDetails.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload
+            })
+            // Payment functionality
+            .addCase(payOrder.pending, (state, action) => {
+                state.loading = true;
+            })
+            .addCase(payOrder.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+            })
+            .addCase(payOrder.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload
+            })
+            // Logged in user orders
+            .addCase(getMyOrders.pending, (state, action) => {
+                state.loading = true;
+            })
+            .addCase(getMyOrders.fulfilled, (state, action) => {
+                state.loading = false;
+                state.myOrders = action.payload;
+            })
+            .addCase(getMyOrders.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload
             })
