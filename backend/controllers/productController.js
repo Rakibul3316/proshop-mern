b53acc54748ea6f37178c44f613476fe94ff2b06
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-02-18 01:45:37 +0600

    add createProduct & updateProduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -48,8 +48,77 @@ const deleteProduct = asyncHandler(async (req, res) => {
 
 })
 
+// @desc        Create product
+// @route       CREATE /api/products
+// @access      Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+    const {
+        product_name,
+        product_price,
+        product_image,
+        product_brand,
+        product_category,
+        product_stock_count,
+        product_description
+
+    } = req.body;
+    const product = new productData({
+        product_name: product_name,
+        product_price: product_price,
+        user_id: req.user._id,
+        product_image: product_image,
+        product_brand: product_brand,
+        product_category: product_category,
+        product_stock_count: product_stock_count,
+        product_description: product_description
+    })
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+})
+
+// @desc        Update product
+// @route       PUT /api/products/:id
+// @access      Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+    const {
+        product_name,
+        product_price,
+        product_image,
+        product_brand,
+        user_id,
+        product_category,
+        product_stock_count,
+        number_of_reviews,
+        product_description
+
+    } = req.body;
+
+    const product = await productData.findById(req.params.id);
+
+    if (product) {
+        product.product_name = product_name;
+        product.product_price = product_price;
+        product.product_image = product_image;
+        product.product_brand = product_brand;
+        product.user_id = user_id;
+        product.product_category = product_category;
+        product.product_stock_count = product_stock_count;
+        product.number_of_reviews = number_of_reviews;
+        product.product_description = product_description;
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
 export {
     getProducts,
     getProductById,
-    deleteProduct
+    deleteProduct,
+    createProduct,
+    updateProduct
 };
