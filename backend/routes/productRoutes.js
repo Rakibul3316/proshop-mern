11f8744298a5fb78ef54072ffce5fa3ea0fce022
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-02-17 23:15:55 +0600

    add deleteProduct controller as /:id route.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
+const router = express.Router();
+
 // Controllers
 import {
     getProducts,
-    getProductById
+    getProductById,
+    deleteProduct
 } from "../controllers/productController.js";
-const router = express.Router();
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById)
-
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
