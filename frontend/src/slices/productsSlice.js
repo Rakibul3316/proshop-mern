afc8c74dc26878ed3f7692c705cc79a527ca3072
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-02-17 23:18:02 +0600

    add deleteProduct thunk

--- frontend/src/slices/productsSlice.js
+++ frontend/src/slices/productsSlice.js
@@ -1,6 +1,13 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const initialState = {
+    loading: false,
+    products: [],
+    error: null,
+    success: false
+}
+
 // First, create the thunk
 export const fetchProducts = createAsyncThunk('fetchProducts', async (_, { rejectWithValue }) => {
     try {
@@ -13,14 +20,27 @@ export const fetchProducts = createAsyncThunk('fetchProducts', async (_, { rejec
             return rejectWithValue(error.message);
         }
     }
-}
-)
+})
 
-const initialState = {
-    loading: false,
-    products: [],
-    error: null
-}
+export const deleteProduct = createAsyncThunk('deleteProduct', async ({ id }, { rejectWithValue, getState }) => {
+    try {
+        const { userInfo } = getState().userLogIn;
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            }
+        }
+
+        const { data } = await axios.delete(`/api/products/${id}`, config)
+        return data;
+    } catch (error) {
+        if (error.response && error.response.data.message) {
+            return rejectWithValue(error.response.data.message);
+        } else {
+            return rejectWithValue(error.message);
+        }
+    }
+})
 
 export const productsSlice = createSlice({
     name: 'products',
@@ -35,8 +55,20 @@ export const productsSlice = createSlice({
             .addCase(fetchProducts.fulfilled, (state, action) => {
                 state.loading = false;
                 state.products = action.payload;
+                state.success = false
             })
             .addCase(fetchProducts.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload
+            })
+            .addCase(deleteProduct.pending, (state, action) => {
+                state.loading = true;
+            })
+            .addCase(deleteProduct.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+            })
+            .addCase(deleteProduct.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload
             })
