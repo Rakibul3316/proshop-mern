26facb3aad681a99dd76c268cd1dded89d07382b
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-02-17 23:14:21 +0600

    change model to models

--- backend/seeder.js
+++ backend/seeder.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import users from './data/user.js';
 import products from './data/products.js';
-import userData from './model/userModel.js';
-import productData from './model/productModel.js';
-import orderData from './model/orderModel.js';
+import userData from './models/userModel.js';
+import productData from './models/productModel.js';
+import orderData from './models/orderModel.js';
 import connectDB from './config/db.js'
 
 dotenv.config()
