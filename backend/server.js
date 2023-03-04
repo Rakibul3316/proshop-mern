cfbd0bde23276530380bdf5f1ce274ccce2bef70
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-03-04 09:46:49 +0600

    config cludinary, add imageRoutes.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import colors from 'colors'
+import colors from 'colors';
+import cloudinary from 'cloudinary';
+
 // Middleware
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
@@ -9,18 +11,27 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import imageRoutes from './routes/imageRoutes.js';
 
 // Initialize app
 const app = express()
 
 // Body parser
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Dot evn configaration
 dotenv.config()
 
 // Connected with Database
 connectDB();
+
+// Configer cloudinary
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 // Initila Route
 app.get('/', (req, res) => {
@@ -31,6 +42,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/image', imageRoutes);
 
 // Error Middleware
 app.use(notFound)
