7085216c580744bbfc35aebd30ff0795aacf6114
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-03-04 09:51:06 +0600

    change img source

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -12,7 +12,11 @@ const Product = ({ product }) => {
       <Card className="mb-4" style={{ borderRadius: "5px" }}>
         <Link to={`/products/${product._id}`}>
           <Card.Img
-            src={product.product_image}
+            src={
+              product.product_image.image_url
+                ? product.product_image.image_url
+                : product.product_image
+            }
             variant="top"
             style={{ padding: "15px", paddingBottom: "0px" }}
           />
