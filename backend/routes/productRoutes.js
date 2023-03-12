fc5a241c0be417c4b0b4f23eef971e91e3ea8f02
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-03-12 09:14:36 +0600

    add /image/delete/:id & /image/update/:id

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,11 +7,16 @@ import {
     getProductById,
     deleteProduct,
     createProduct,
-    updateProduct
+    updateProduct,
+    deleteProductImage,
+    updateProductImage
 } from "../controllers/productController.js";
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct);
+router.route('/image/delete/:id').post(protect, admin, deleteProductImage,
+);
+router.route('/image/update/:id').post(protect, admin, updateProductImage);
 
 export default router;
