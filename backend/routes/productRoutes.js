0bce6ace72983065a0493bdb17a5518eb450949a
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2023-03-18 08:13:24 +0600

    add product review route.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,12 +9,14 @@ import {
     createProduct,
     updateProduct,
     deleteProductImage,
-    updateProductImage
+    updateProductImage,
+    createProductReview
 } from "../controllers/productController.js";
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 router.route('/image/delete/:id').post(protect, admin, deleteProductImage,
 );
 router.route('/image/update/:id').post(protect, admin, updateProductImage);
